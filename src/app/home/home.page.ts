9a3e66a0ca7ad4eb09acc17e8586858aacfc899c
AntonioJVera11 <arevmedia98@gmail.com>
2019-11-12 13:17:00 +0100

    Falta modificar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
-
 import { FirestoreService } from '../firestore.service';
-
 import { Piezas } from '../piezas';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
   piezaEditando: Piezas;
+  idPiezaSelec: string;
 
   constructor(private firestoreService: FirestoreService) {
     //Creamos una pieza vacía
     this.piezaEditando = {} as Piezas;
+
+    //Obtenemos la lista de tareas
+    this.obtenerListaPiezas();
   }
+
+  
+  arrayColeccionPiezas: any = [{
+    id: "",
+    data: {} as Piezas
+   }];
+
 
   clicBotonInsertar() {
     this.firestoreService.insertar("piezas", this.piezaEditando).then(() => {
@@ -25,6 +37,35 @@ export class HomePage {
     }, (error) => {
       console.error(error);
     });
+  }
+
+  clicBotonBorrar() {
+    this.firestoreService.borrar("piezas", this.idPiezaSelec).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaPiezas();
+      // Limpiar datos de pantalla
+      this.piezaEditando = {} as Piezas;
+    })
+  }
+
+  obtenerListaPiezas(){
+    this.firestoreService.consultar("piezas").subscribe((resultadoConsultaPiezas) => {
+      this.arrayColeccionPiezas = [];
+      resultadoConsultaPiezas.forEach((datosPiezas: any) => {
+        this.arrayColeccionPiezas.push({
+          id: datosPiezas.payload.doc.id,
+          data: datosPiezas.payload.doc.data()
+        });
+      })
+    });
+  }
+
+  selecPieza(piezaSelec) {
+    console.log("Pieza seleccionada: ");
+    console.log(piezaSelec);
+    this.idPiezaSelec = piezaSelec.id;
+    this.piezaEditando.titulo = piezaSelec.data.titulo;
+    this.piezaEditando.descripcion = piezaSelec.data.descripcion;
   }
 
 }
