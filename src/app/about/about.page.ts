d973ab7dd8dbaf02c8d6616116823720c2efb045
AntonioJVera11 <alum.antonio.vera.munoz@ieslosremedios.org>
2020-02-22 12:18:57 +0100

    Mapa implementado.

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import {Map,tileLayer,marker} from 'leaflet';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+  map:Map;
+  newMarker:any;
+  address:string[];
 
   constructor(private router: Router) { }
+
+    // The below function is added
+    ionViewDidEnter(){
+      this.loadMap();
+    }
+   // The below function is added
+   loadMap(){this.map = new Map("mapId").setView([36.6772,-5.4461], 14);
+   tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+   { attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY- SA</a>'})
+   .addTo(this.map); // This line is added to add the Tile Layer to our map
+  }
 
   ngOnInit() {
   }
